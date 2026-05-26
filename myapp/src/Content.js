e876bdb117fa68be87f1eccda55e05cfea66e5e7
dhwani <dhwanis230@gmail.com>
2026-05-26 11:37:52 +0530

    node js

---
+++ myapp/src/Content.js
@@ -0,0 +1,12 @@
+function Content(){
+    return(
+        <div>
+            <h1>
+                Welcome to My App
+            </h1>
+            <p>This is Content Component </p>
+        </div>
+    );
+}
+
+export default Content;
